32785dc804f35904a587529ef6e1b8bc6c4dfefd
Max Green <strixdevelop@gmail.com>
2019-11-03 23:13:27 +0300

    travis-ci config

--- tests/State.test.ts
+++ tests/State.test.ts
@@ -4,6 +4,7 @@ describe('State', () => {
 
     it('isPure static method', () => {
         const { isPure } = State;
+        // pure
         expect(isPure('some string')).toBe(true);
         expect(isPure(666)).toBe(true);
         expect(isPure(666.666)).toBe(true);
@@ -12,5 +13,11 @@ describe('State', () => {
         expect(isPure(undefined)).toBe(true);
         expect(isPure(NaN)).toBe(true);
         expect(isPure(null)).toBe(true);
+        // not pure
+        expect(isPure(() => {})).toBe(false);
+        expect(isPure(/w/)).toBe(false);
+        expect(isPure(Symbol('sym'))).toBe(false);
+        expect(isPure({})).toBe(false);
+        expect(isPure([])).toBe(false);
     })
 });
