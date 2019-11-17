fa9ae07a00be811e87ff92d83b2c6f70c8b45b20
Max Green <strixdevelop@gmail.com>
2019-11-17 16:01:59 +0300

    State

--- src/State.ts
+++ src/State.ts
@@ -1,7 +1,22 @@
+type PureType = string|number|boolean|undefined|null;
+interface FlatState {
+    [key: string]: PureType;
+};
+interface Config {
+    pathSeparator: string;
+    typeSeparator: string;
+}
 const pureTypes = ['string', 'number', 'boolean'];
+
 export default class State {
-    _state: any;
-    constructor(state: any) {
+    private _state: FlatState | PureType = {};
+    private _config: Config = {
+        pathSeparator: '/',
+        typeSeparator: '#',
+    };
+
+    constructor(rawState: unknown) {
+        const {state} = State.makeFlatState(rawState);
         this._state = state;
     }
 
@@ -11,8 +26,68 @@ export default class State {
             || value === undefined
             || value === null
         ) {
+            value as PureType;
             return true;
         }
         return false;
+    }
+
+    static makeFlatState(
+        rawState: unknown | {[key: string]: unknown}, 
+        rootState: FlatState = {}, 
+        pathSeparator = '/', 
+        typeSeparator = '#', 
+        pathTo: Array<string> = []
+    ): {
+        state: FlatState | PureType; 
+        statePath: Array<string> | null;
+    } {
+        let type: 'array' | 'object' | null = null;
+        
+        if (State.isPure(rawState)) {
+            const state = rawState as PureType;
+            
+            return {
+                state,
+                statePath: pathTo,
+            };
+        }
+        else if (Array.isArray(rawState)) {
+            type = 'array';
+        }
+        else if (rawState.toString() === '[object Object]') {
+            type = 'object';
+        }
+
+        if (type === null) {
+            return {
+                state: undefined,
+                statePath: pathTo,
+            };
+        }
+        else {
+            let path = [...pathTo];
+            path[path.length - 1] = `${type}${typeSeparator}${path[path.length - 1]}`;
+            if (!path.length) {
+                path = [`${type}${typeSeparator}`];
+            }
+            const keys = Object.keys(rawState);
+            keys.forEach((key) => {
+                const {state, statePath} = State.makeFlatState(
+                    (rawState as {[key: string]: unknown})[key],
+                    rootState,
+                    pathSeparator,
+                    typeSeparator,
+                    [...path, key],
+                );
+                if (statePath.length) {
+                    rootState[statePath.join(pathSeparator)] = state as PureType;
+                }
+            });
+            return {
+                state: rootState,
+                statePath: [],
+            };
+        }
     }
 }
--- tests/State.test.ts
+++ tests/State.test.ts
@@ -1,23 +1,75 @@
 import State from '@src/State';
 
-describe('State', () => {
+function stringifyState(state: unknown): string {
+    return JSON.stringify(state, undefined, '    ');
+}
 
-    it('isPure static method', () => {
+describe('State', () => {
+    const pureValues = ['state string', 666, 666.666, true, false, undefined, NaN, null];
+    const validEntities = [[], {}];
+    const notValidEntities = [Symbol('sym'), /w/, () => {}];
+    const entities = [...validEntities, ...notValidEntities];
+
+    describe('isPure static method', () => {
         const { isPure } = State;
-        // pure
-        expect(isPure('some string')).toBe(true);
-        expect(isPure(666)).toBe(true);
-        expect(isPure(666.666)).toBe(true);
-        expect(isPure(true)).toBe(true);
-        expect(isPure(false)).toBe(true);
-        expect(isPure(undefined)).toBe(true);
-        expect(isPure(NaN)).toBe(true);
-        expect(isPure(null)).toBe(true);
-        // not pure
-        expect(isPure(() => {})).toBe(false);
-        expect(isPure(/w/)).toBe(false);
-        expect(isPure(Symbol('sym'))).toBe(false);
-        expect(isPure({})).toBe(false);
-        expect(isPure([])).toBe(false);
-    })
+        it('with pure', () => {
+            for (const value of pureValues) {
+                expect(isPure(value)).toBe(true);
+            }
+        });
+        it('with entities', () => {
+            for (const value of entities) {
+                expect(isPure(value)).toBe(false);
+            }
+        });
+    });
+
+    describe('makeFlatState static method', () => {
+        const { makeFlatState } = State;
+        it ('with pure', () => {
+            for (const value of pureValues) {
+                const flatState = makeFlatState(value);
+                expect(flatState).toEqual({
+                    state: value,
+                    statePath: []
+                });
+            }
+        });
+        it('with valid entities', () => {
+            for (const value of validEntities) {
+                const flatState = makeFlatState(value);
+                expect(flatState).toEqual({
+                    state: {},
+                    statePath: [],
+                });
+            }
+        });
+        it('with not valid entities', () => {
+            for (const value of notValidEntities) {
+                const flatState = makeFlatState(value);
+                expect(flatState).toEqual({
+                    state: undefined,
+                    statePath: [],
+                });
+            }
+        });
+        it('with nested object in array', () => {
+            const rawState = [1, '2', false, /w/, [3],
+                {
+                    propA: 1,
+                    propB: 'some prop',
+                    propC: true,
+                    propD: () => {},
+                    propE: ['1'],
+                    propF: {
+                        someProp: 1,
+                    },
+                }
+            ];
+            const flatState = makeFlatState(rawState);
+            console.log(flatState);
+            expect(Object.keys(flatState.state)).toHaveLength(11);
+            expect(stringifyState(flatState)).toMatchSnapshot();
+        });
+    });
 });
